7c23b207a2095ca7e62302df1f13a8fc2d9ec51a
Brian Henry <BrianHenryIE@gmail.com>
2025-02-15 17:33:24 -0800

    Add `bootstrap.php` to `files` autoloader to load `autoload_aliases.php`

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,10 @@
 <?php
+/**
+ * When strauss is installed via Composer, this will help load the aliases file.
+ */
 
-if(file_exists('vendor/composer/autoload_aliases.php')) {
-    require_once 'vendor/composer/autoload_aliases.php';
+$autoloadAliasesFilepath = __DIR__ . '/../../composer/autoload_aliases.php';
+if(file_exists($autoloadAliasesFilepath)) {
+    require_once $autoloadAliasesFilepath;
 }
+unset($autoloadAliasesFilepath);
