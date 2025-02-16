75d742bfaf8d2ddd6cb403821b8453762a9cba8d
Brian Henry <BrianHenryIE@gmail.com>
2025-02-15 18:24:20 -0800

    Update name / namespace

--- tests/Unit/PrefixerTest.php
+++ tests/Unit/PrefixerTest.php
@@ -12,6 +12,7 @@ use BrianHenryIE\Strauss\Config\PrefixerConfigInterface;
 use BrianHenryIE\Strauss\Files\File;
 use BrianHenryIE\Strauss\Pipeline\Prefixer;
 use BrianHenryIE\Strauss\TestCase;
+use BrianHenryIE\Strauss\Tests\Issues\MozartIssue93Test;
 use BrianHenryIE\Strauss\Types\ClassSymbol;
 use BrianHenryIE\Strauss\Types\ConstantSymbol;
 use BrianHenryIE\Strauss\Types\DiscoveredSymbols;
@@ -336,7 +337,7 @@ EOD;
 
 
     /**
-     * @see ClassmapReplacerIntegrationTest::test_it_does_not_make_classname_replacement_inside_namespaced_file()
+     * @see MozartIssue93Test
      * @see https://github.com/coenjacobs/mozart/issues/93
      *
      * @author BrianHenryIE
@@ -370,7 +371,7 @@ EOD;
     }
 
     /**
-     * @see ClassmapReplacerIntegrationTest::test_it_does_not_make_classname_replacement_inside_namespaced_file()
+     * @see MozartIssue93Test
      * @see https://github.com/coenjacobs/mozart/issues/93
      *
      * @author BrianHenryIE
