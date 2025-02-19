675d5d89b8db18bde6c827654a9c7aad984559dd
Brian Henry <BrianHenryIE@gmail.com>
2025-02-19 15:52:09 -0800

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,16 @@
  * When strauss is installed via Composer, this will help load the aliases file.
  */
 
-$autoloadAliasesFilepath = __DIR__ . '/../../composer/autoload_aliases.php';
-if(file_exists($autoloadAliasesFilepath)) {
+$autoloadAliasesFilepath = realpath(__DIR__ . '/../../composer/autoload_aliases.php');
+if (file_exists($autoloadAliasesFilepath)) {
+    // TODO: This will only work for default configuration; read the composer.json file to determine the target directory.
+    // Check it's not trying to load the vendor/autoload.php file that is currently being loaded.
+    $autoloadTargetDirFilepath = realpath(__DIR__ . '/../../../vendor-prefixed/autoload.php');
+    if ($autoloadTargetDirFilepath !== realpath(__DIR__ . '/../../autoload.php') && file_exists($autoloadTargetDirFilepath)) {
+        require_once $autoloadTargetDirFilepath;
+    }
+    unset($autoloadTargetDirFilepath);
+
     require_once $autoloadAliasesFilepath;
 }
 unset($autoloadAliasesFilepath);
