225bbf78720ca875d677e541c04319e1c1559c39
Brian Henry <BrianHenryIE@gmail.com>
2025-02-18 22:33:35 -0800

    Add `TestCase::getFileSystem()`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,11 @@
 
 namespace BrianHenryIE\Strauss;
 
+use BrianHenryIE\Strauss\Helpers\FileSystem;
+use Elazar\Flystream\FilesystemRegistry;
+use League\Flysystem\Config;
+use League\Flysystem\InMemory\InMemoryFilesystemAdapter;
+
 class TestCase extends \PHPUnit\Framework\TestCase
 {
     public static function assertEqualsRN($expected, $actual, string $message = ''): void
@@ -29,5 +34,45 @@ class TestCase extends \PHPUnit\Framework\TestCase
         $string = preg_replace('/^\s*/m', '', $string);
         $string = preg_replace('/\n\s*\n/', "\n", $string);
         return $string;
+    }
+
+    protected function getFileSystem(): FileSystem
+    {
+
+        $inMemoryFilesystem = new \BrianHenryIE\Strauss\Helpers\InMemoryFilesystemAdapter();
+
+        $filesystem = new Filesystem(
+            new \League\Flysystem\Filesystem(
+                $inMemoryFilesystem,
+                [
+                    Config::OPTION_DIRECTORY_VISIBILITY => 'public',
+                ],
+                new \Elazar\Flystream\StripProtocolPathNormalizer()
+            )
+        );
+
+        /** @var FilesystemRegistry $registry */
+        $registry = \Elazar\Flystream\ServiceLocator::get(\Elazar\Flystream\FilesystemRegistry::class);
+        // Register a file stream mem:// to handle file operations by third party libraries.
+        // This exception handling probably doesn't matter in real life but does in unit tests.
+        try {
+            $registry->get('mem');
+        } catch (\Exception $e) {
+            $registry->register('mem', $filesystem);
+        }
+
+        return $filesystem;
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        /** @var FilesystemRegistry $registry */
+        try {
+            $registry = \Elazar\Flystream\ServiceLocator::get(\Elazar\Flystream\FilesystemRegistry::class);
+            $registry->unregister('mem');
+        } catch (\Exception $e) {
+        }
     }
 }
