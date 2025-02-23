42840b570d503828d4722aa422a5e9926910a6cc
Brian Henry <BrianHenryIE@gmail.com>
2025-02-23 00:10:43 -0800

    Add test to ensure `autoload_aliases.php` is only added once

--- tests/Integration/Autoload/VendorComposerAutoloadFixtureTest.php
+++ tests/Integration/Autoload/VendorComposerAutoloadFixtureTest.php
@@ -78,7 +78,7 @@ EOD;
         $this->assertStringNotContainsString('autoload_aliases.php', $composerAutoloadString);
     }
 
-    public function testRepeatedlyRunningOnlyAddsOnce(): void
+    public function testRepeatedlyRunningOnlyAddsAutoloadOnce(): void
     {
         $composerJsonString = <<<'EOD'
 {
@@ -116,6 +116,48 @@ EOD;
         $this->assertEquals(
             1,
             substr_count($composerAutoloadString, "require_once __DIR__ . '/../vendor-prefixed/autoload.php'"),
+            $composerAutoloadString
+        );
+    }
+
+    public function testRepeatedlyRunningOnlyAddsAutoloadAliasesOnce(): void
+    {
+        $composerJsonString = <<<'EOD'
+{
+  "name": "brianhenryie/strauss",
+  "require": {
+    "league/container": "*"
+  },
+  "extra": {
+    "strauss": {
+      "namespace_prefix": "BrianHenryIE\\Strauss\\",
+      "classmap_prefix": "BrianHenryIE_Strauss_",
+      "delete_vendor_packages": true
+    }
+  }
+}
+EOD;
+
+        file_put_contents($this->testsWorkingDir . 'composer.json', $composerJsonString);
+
+        chdir($this->testsWorkingDir);
+
+        exec('composer install');
+
+        $exitCode = $this->runStrauss($output, 'include-autoloader');
+        assert(0 === $exitCode, $output);
+
+        $exitCode = $this->runStrauss($output, 'include-autoloader');
+        assert(0 === $exitCode, $output);
+
+        $exitCode = $this->runStrauss($output, 'include-autoloader');
+        assert(0 === $exitCode, $output);
+
+        $composerAutoloadString = file_get_contents($this->testsWorkingDir . 'vendor/autoload.php');
+
+        $this->assertEquals(
+            1,
+            substr_count($composerAutoloadString, "require_once __DIR__ . '/composer/autoload_aliases.php'"),
             $composerAutoloadString
         );
     }
