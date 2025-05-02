722251672347f99537aae18f80864d531c45f4cd
Rik der Kinderen <r.derkinderen@juist.nl>
2025-05-02 11:59:38 +0200

    filter 'implements' nodes on FullyQualified + add issue test

---
+++ tests/Issues/StraussIssue172Test.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Improper dealing with global namespaces
+ *
+ * @see https://github.com/BrianHenryIE/strauss/issues/172
+ */
+
+namespace BrianHenryIE\Strauss\Tests\Issues;
+
+use BrianHenryIE\Strauss\Tests\Integration\Util\IntegrationTestCase;
+
+/**
+ * @package BrianHenryIE\Strauss\Tests\Issues
+ * @coversNothing
+ */
+class StraussIssue172Test extends IntegrationTestCase
+{
+    public function test_issue_172()
+    {
+        $composerJsonString = <<<'EOD'
+{
+  "name": "issue/80",
+  "require": {
+    "guzzlehttp/guzzle": "7.9.3"
+  },
+  "extra": {
+    "strauss": {
+      "namespace_prefix": "Company\\Project\\"
+    }
+  }
+}
+EOD;
+
+        chdir($this->testsWorkingDir);
+
+        file_put_contents($this->testsWorkingDir . '/composer.json', $composerJsonString);
+
+        exec('composer install --no-dev');
+        $exitCode = $this->runStrauss($output);
+        assert(0 === $exitCode, $output);
+
+        $php_string = file_get_contents($this->testsWorkingDir . 'vendor-prefixed/guzzlehttp/guzzle/src/Client.php');
+
+        self::assertStringContainsString("class Client implements ClientInterface, \Company\Project\Psr\Http\Client\ClientInterface", $php_string);
+    }
+}
