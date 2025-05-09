58cabe64b7ea3f2f55850c2e56071b9314ddf84a
Brian Henry <BrianHenryIE@gmail.com>
2025-05-08 23:51:12 -0700

    Update DumpAutoloadTest.php

--- tests/Unit/Pipeline/Autoload/DumpAutoloadTest.php
+++ tests/Unit/Pipeline/Autoload/DumpAutoloadTest.php
@@ -30,15 +30,15 @@ class DumpAutoloadTest extends \BrianHenryIE\Strauss\TestCase
 //        $config->expects('getTargetDirectory')->times(2)->andReturn('project/vendor-prefixed/');
         $config->expects('getTargetDirectory')->times(4)->andReturn('project/vendor-prefixed/');
 //        $config->expects('getNamespacePrefix')->once()->andReturn('BrianHenryIE\\Test\\');
-        $config->expects('getNamespacePrefix')->times(9)->andReturn('BrianHenryIE\\Test\\');
+        $config->expects('getNamespacePrefix')->times(8)->andReturn('BrianHenryIE\\Test\\');
 
-        $config->expects('getVendorDirectory')->once()->andReturn('project/vendor/');
-        $config->expects('getExcludeNamespacesFromCopy')->once()->andReturn([]);
-        $config->expects('getExcludePackagesFromCopy')->once()->andReturn([]);
-        $config->expects('getExcludeFilePatternsFromCopy')->once()->andReturn([]);
-        $config->expects('getClassmapPrefix')->times(7)->andReturn('BrianHenryIE_Test_');
-        $config->expects('getConstantsPrefix')->times(21)->andReturn('BRIANHENRYIE_TEST_');
-        $config->expects('getExcludeNamespacesFromPrefixing')->times(7)->andReturn([]);
+        $config->expects('getVendorDirectory')->times(2)->andReturn('project/vendor/');
+        $config->expects('getExcludeNamespacesFromCopy')->times(2)->andReturn([]);
+        $config->expects('getExcludePackagesFromCopy')->times(2)->andReturn([]);
+        $config->expects('getExcludeFilePatternsFromCopy')->times(2)->andReturn([]);
+        $config->expects('getClassmapPrefix')->times(6)->andReturn('BrianHenryIE_Test_');
+        $config->expects('getConstantsPrefix')->times(18)->andReturn('BRIANHENRYIE_TEST_');
+        $config->expects('getExcludeNamespacesFromPrefixing')->times(6)->andReturn([]);
 
         /** @var FileSystem $filesystem */
         $filesystem = $this->getFileSystem();
