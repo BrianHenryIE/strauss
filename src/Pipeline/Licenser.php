f7a8f415af6750b4e97e1f2fae5934e33980886c
Krzysztof Grabania <krzysztof@grabania.pl>
2025-05-09 11:02:55 +0200

    fix: skip directories when searching for license files

--- src/Pipeline/Licenser.php
+++ src/Pipeline/Licenser.php
@@ -20,6 +20,7 @@ use BrianHenryIE\Strauss\Composer\ComposerPackage;
 use BrianHenryIE\Strauss\Composer\Extra\StraussConfig;
 use BrianHenryIE\Strauss\Helpers\FileSystem;
 use League\Flysystem\FilesystemException;
+use League\Flysystem\StorageAttributes;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
@@ -140,7 +141,8 @@ class Licenser
         foreach ($this->dependencies as $dependency) {
             $packagePath = $dependency->getPackageAbsolutePath();
 
-            $files = $this->filesystem->listContents($packagePath, true);
+            $files = $this->filesystem->listContents($packagePath, true)
+                ->filter(fn (StorageAttributes $attributes) => $attributes->isFile());;
             foreach ($files as $file) {
                 $filePath = '/' . $file->path();
 
