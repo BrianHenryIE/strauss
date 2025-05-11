c5150d870f3e10c8719eb1218ac956a3e27e53aa
Brian Henry <BrianHenryIE@gmail.com>
2025-05-10 17:48:35 -0700

    Add test exploring `allow_url_include` issue

--- tests/Issues/StraussIssue183Test.php
+++ tests/Issues/StraussIssue183Test.php
@@ -75,4 +75,63 @@ EOD;
 
         $this->assertEquals(0, $result_code, $outputString);
     }
+
+    public function test_allow_url_include(): void
+    {
+        $composerJsonString = <<<EOD
+{
+  "name": "strauss/issue183",
+  "require": {
+    "psr/log": "*"
+  },
+  "require-dev": {
+    "brianhenryie/strauss": "dev-master"
+  },
+  "extra": {
+    "strauss": {
+      "namespace_prefix": "Strauss\\\\Issue183\\\\",
+      "delete_vendor_packages": true
+    }
+  }
+}
+EOD;
+
+        chdir($this->testsWorkingDir);
+
+        file_put_contents($this->testsWorkingDir . '/composer.json', $composerJsonString);
+
+        exec('composer install');
+
+        $exitCode = $this->runStrauss($output);
+        assert(0 === $exitCode, $output);
+
+        // Directive 'allow_url_include' is deprecated
+
+        // php -r "print_r(ini_get_all()['allow_url_include']);"
+        // php -d allow_url_include=on -r "print_r(ini_get_all()['allow_url_include']);"
+        // php -d allow_url_include=off -r "print_r(ini_get_all()['allow_url_include']);"
+
+        // php -r "include __DIR__ . '/vendor/autoload.php'; new class() { use \Psr\Log\LoggerAwareTrait; };"
+
+        // Get the loaded PHP ini file
+        // php --ini | grep Loaded | grep -o '\S*$'
+        // PHP_INI_FILE=$(php --ini | grep Loaded | grep -o '\S*$')
+        // cat $PHP_INI_FILE | grep allow_url_include
+
+        // macOS
+        // sed -i '' 's/allow_url_include = Off/allow_url_include = On/g' $PHP_INI_FILE
+        // sed -i '' 's/allow_url_include = On/allow_url_include = Off/g' $PHP_INI_FILE
+
+        // Deprecated: Directive 'allow_url_include' is deprecated in Unknown on line 0
+
+        // https://www.php.net/manual/en/filesystem.configuration.php#ini.allow-url-include
+
+        // php -d allow_url_include=on -d error_reporting="E_ALL & ~E_DEPRECATED" vendor/bin/strauss
+
+        exec('php -d allow_url_include=on -d error_reporting="E_ALL & ~E_DEPRECATED" -r "include __DIR__ . \'/vendor/autoload.php\'; new class() { use \Psr\Log\LoggerAwareTrait; };" 2>&1', $output, $result_code);
+        $outputString = implode(PHP_EOL, $output);
+
+        $this->assertEmpty($outputString, $outputString);
+        $this->assertEquals(0, $result_code, $outputString);
+    }
 }
