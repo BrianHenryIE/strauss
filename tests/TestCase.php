81a0d1a6a9b37d86618581c4511f96852ba24859
Brian Henry <BrianHenryIE@gmail.com>
2025-05-15 20:08:22 -0700

    Add `::assertStringContainsStringRemoveBlankLinesLeadingWhitespace()`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -31,6 +31,15 @@ class TestCase extends \PHPUnit\Framework\TestCase
         );
     }
 
+    public static function assertStringContainsStringRemoveBlankLinesLeadingWhitespace($expected, $actual, string $message = ''): void
+    {
+        self::assertStringContainsString(
+            self::stripWhitespaceAndBlankLines($expected),
+            self::stripWhitespaceAndBlankLines($actual),
+            $message
+        );
+    }
+
     protected static function stripWhitespaceAndBlankLines(string $string): string
     {
         $string = str_replace("\r\n", "\n", $string);
