9bbb4ae8548d0c398a1a9027c1917818bbaa12b2
Brian Henry <BrianHenryIE@gmail.com>
2025-06-10 01:17:31 -0700

    Update StraussIssue143Test.php

--- tests/Issues/StraussIssue143Test.php
+++ tests/Issues/StraussIssue143Test.php
@@ -127,7 +127,7 @@ EOD;
      */
     public function test_silent_option_symfony_72(): void
     {
-        $this->markTestSkippedOnPhpVersionEqualOrAbove('8.2');
+        $this->markTestSkippedOnPhpVersionBelow('8.2');
 
         $composerJsonString = <<<'EOD'
 {
