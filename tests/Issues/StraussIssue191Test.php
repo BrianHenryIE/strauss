148a890baa256a7150e6531c85e0812df2853fb8
Brian Henry <BrianHenryIE@gmail.com>
2025-06-18 13:32:39 -0700

    Create StraussIssue191Test.php

---
+++ tests/Issues/StraussIssue191Test.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Really an issue in brianhenryie/simple-php-code-parser; this is a regression test
+ *
+ * @see https://github.com/BrianHenryIE/strauss/issues/191
+ * @see https://github.com/BrianHenryIE/Simple-PHP-Code-Parser/pull/8
+ * @see https://github.com/BrianHenryIE/Simple-PHP-Code-Parser/releases/tag/0.15.1
+ */
+
+namespace BrianHenryIE\Strauss\Tests\Issues;
+
+use BrianHenryIE\Strauss\Tests\Integration\Util\IntegrationTestCase;
+
+/**
+ * @package BrianHenryIE\Strauss\Tests\Issues
+ * @coversNothing
+ */
+class StraussIssue191Test extends IntegrationTestCase
+{
+    public function test_fatal(): void
+    {
+        $composerJsonString = <<<'EOD'
+{
+  "name": "adam/strauss-error",
+  "description": "Minimal example for bug report",
+  "type": "project",
+  "license": "MIT",
+  "autoload": {
+    "psr-4": {
+      "Adam\\StraussError\\": "src/"
+    }
+  },
+  "require": {
+    "league/mime-type-detection": "*"
+  }
+}
+
+EOD;
+
+        chdir($this->testsWorkingDir);
+
+        file_put_contents($this->testsWorkingDir . '/composer.json', $composerJsonString);
+
+        exec('composer install');
+
+        $exitCode = $this->runStrauss($output);
+        assert(0 === $exitCode, $output);
+
+        $this->assertStringNotContainsString(
+            "Couldn't find constant \\League\\MimeTypeDetection\\FinfoMimeTypeDetector::INCONCLUSIVE_MIME_TYPES",
+            $this->getActualOutput()
+        );
+    }
+}
