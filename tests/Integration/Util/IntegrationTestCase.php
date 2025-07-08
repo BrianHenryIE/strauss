1d981c09f88664c85576da307a59ac1112539372
Brian Henry <BrianHenryIE@gmail.com>
2025-07-07 22:30:04 -0700

    Add env to `::runStrauss()` method

--- tests/Integration/Util/IntegrationTestCase.php
+++ tests/Integration/Util/IntegrationTestCase.php
@@ -80,11 +80,11 @@ class IntegrationTestCase extends TestCase
         return false;
     }
 
-    protected function runStrauss(?string &$allOutput = null, string $params = ''): int
+    protected function runStrauss(?string &$allOutput = null, string $params = '', string $env = ''): int
     {
         if (file_exists($this->projectDir . '/strauss.phar')) {
             // TODO add xdebug to the command
-            exec('php ' . $this->projectDir . '/strauss.phar ' . $params, $output, $return_var);
+            exec($env . ' php ' . $this->projectDir . '/strauss.phar ' . $params, $output, $return_var);
             $allOutput = implode(PHP_EOL, $output);
             echo $allOutput;
             return $return_var;
@@ -106,6 +106,14 @@ class IntegrationTestCase extends TestCase
         }
 
         $this->logger && $strauss->setLogger($this->logger);
+
+        $envArray = array_map(
+            fn(string $pair) => explode('=', $pair),
+            explode(' ', $env)
+        );
+        foreach ($envArray as $var) {
+            $_ENV[$var[0]] = $var[1];
+        }
 
         $argv = array_merge(['strauss'], array_filter($paramsSplit));
         $inputInterface = new ArgvInput($argv);
