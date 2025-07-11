237f573d2159fcbf4bfbe0a3dafd3bcaff701ba6
Brian Henry <BrianHenryIE@gmail.com>
2025-07-10 17:32:55 -0700

    Extract `autoload_aliases.template.php`

--- src/Pipeline/Aliases/Aliases.php
+++ src/Pipeline/Aliases/Aliases.php
@@ -53,123 +53,27 @@ class Aliases
                 $autoloadAliasesFunctionsString
                 GLOBAL;
 
-        return <<<TEMPLATE
-                <?php
-                
-                $globalFunctionsString
-                
-                // Everything else – irrelevant that this part is namespaced
-                namespace $namespace {
-                	
-                class AliasAutoloader
-                {
-                	private string \$includeFilePath;
-                
-                    private array \$autoloadAliases = $autoloadAliases;
-                
-                    public function __construct() {
-                		\$this->includeFilePath = __DIR__ . '/autoload_alias.php';
-                    }
-                    
-                    public function autoload(\$class)
-                    {
-                        if (!isset(\$this->autoloadAliases[\$class])) {
-                            return;
-                        }
-                        switch (\$this->autoloadAliases[\$class]['type']) {
-                            case 'class':
-                                \$this->load(
-                                    \$this->classTemplate(
-                                        \$this->autoloadAliases[\$class]
-                                    )
-                                );
-                                break;
-                            case 'interface':
-                                \$this->load(
-                                    \$this->interfaceTemplate(
-                                        \$this->autoloadAliases[\$class]
-                                    )
-                                );
-                                break;
-                            case 'trait':
-                                \$this->load(
-                                    \$this->traitTemplate(
-                                        \$this->autoloadAliases[\$class]
-                                    )
-                                );
-                                break;
-                            default:
-                                // Never.
-                                break;
-                        }
-                    }
-                
-                    private function load(string \$includeFile)
-                    {
-                        file_put_contents(\$this->includeFilePath, \$includeFile);
-                        include \$this->includeFilePath;
-                        file_exists(\$this->includeFilePath) && unlink(\$this->includeFilePath);
-                    }
-                	
-                	// TODO: What if this was a real function in this class that could be used for testing, which would be read and written by php-parser?
-                    private function classTemplate(array \$class): string
-                    {
-                        \$abstract = \$class['isabstract'] ? 'abstract ' : '';
-                        \$classname = \$class['classname'];
-                        if(isset(\$class['namespace'])) {
-                            \$namespace = "namespace {\$class['namespace']};";
-                            \$extends = '\\\\' . \$class['extends'];
-                	        \$implements = empty(\$class['implements']) ? ''
-                	            : ' implements \\\\' . implode(', \\\\', \$class['implements']);
-                        } else {
-                            \$namespace = '';
-                            \$extends = \$class['extends'];
-                	        \$implements = !empty(\$class['implements']) ? ''
-                	            : ' implements ' . implode(', ', \$class['implements']);
-                        }
-                        return <<<EOD
-                				<?php
-                				\$namespace
-                				\$abstract class \$classname extends \$extends \$implements {}
-                				EOD;
-                    }
-                    
-                    private function interfaceTemplate(array \$interface): string
-                    {
-                        \$interfacename = \$interface['interfacename'];
-                        \$namespace = isset(\$interface['namespace']) 
-                            ? "namespace {\$interface['namespace']};" : '';
-                        \$extends = isset(\$interface['namespace'])
-                            ? '\\\\' . implode('\\\\ ,', \$interface['extends'])
-                            : implode(', ', \$interface['extends']);
-                        return <<<EOD
-                				<?php
-                				\$namespace
-                				interface \$interfacename extends \$extends {}
-                				EOD;
-                    } 
-                    private function traitTemplate(array \$trait): string
-                    {
-                        \$traitname = \$trait['traitname'];
-                        \$namespace = isset(\$trait['namespace']) 
-                            ? "namespace {\$trait['namespace']};" : '';
-                        \$uses = isset(\$trait['namespace'])
-                            ? '\\\\' . implode(';' . PHP_EOL . '    use \\\\', \$trait['use'])
-                            : implode(';' . PHP_EOL . '    use ', \$trait['use']);
-                        return <<<EOD
-                				<?php
-                				\$namespace
-                				trait \$traitname { 
-                				    use \$uses; 
-                				}
-                				EOD;
-                	    }
-                	}
-                	
-                	spl_autoload_register( [ new AliasAutoloader(), 'autoload' ] );
+        $template = file_get_contents(__DIR__ . '/autoload_aliases.template.php');
 
-                }
-                TEMPLATE;
+        $template = str_replace(
+            '// FunctionsAndConstants',
+            $globalFunctionsString,
+            $template
+        );
+
+        $template = str_replace(
+            'namespace BrianHenryIE\Strauss {',
+            'namespace ' . $namespace . ' {',
+            $template
+        );
+
+        $template = str_replace(
+            'private array $autoloadAliases = [];',
+            "private array \$autoloadAliases = $autoloadAliases;",
+            $template
+        );
+
+        return $template;
     }
 
     public function writeAliasesFileForSymbols(DiscoveredSymbols $symbols): void
