52cee210a6a1708cc2cbf441011a2cb82108e98c
Brian Henry <BrianHenryIE@gmail.com>
2025-07-15 12:05:55 -0700

    Update VendorComposerAutoloadFeatureTest.php

--- tests/Integration/Autoload/VendorComposerAutoloadFeatureTest.php
+++ tests/Integration/Autoload/VendorComposerAutoloadFeatureTest.php
@@ -164,4 +164,127 @@ EOD;
             $composerAutoloadString
         );
     }
+
+    public function test_does_not_edit_autoloader_namespaces_when_not_deleting_files(): void
+    {
+        $composerJsonString = <<<'EOD'
+{
+  "name": "brianhenryie/strauss",
+  "require": {
+    "league/container": "*"
+  },
+  "extra": {
+    "strauss": {
+      "namespace_prefix": "BrianHenryIE\\Strauss\\",
+      "classmap_prefix": "BrianHenryIE_Strauss_",
+      "target_directory": "vendor-prefixed",
+      "delete_vendor_packages": false
+    }
+  }
+}
+EOD;
+
+        file_put_contents($this->testsWorkingDir . 'composer.json', $composerJsonString);
+
+        chdir($this->testsWorkingDir);
+
+        exec('composer install');
+
+        $exitCode = $this->runStrauss($output);
+        assert(0 === $exitCode, $output);
+
+        $composerAutoloadString = file_get_contents($this->testsWorkingDir . 'vendor/composer/autoload_psr4.php');
+
+        $this->assertStringContainsString(
+            "'League\\\\Container\\\\",
+            $composerAutoloadString
+        );
+
+        $this->assertStringNotContainsString(
+            "BrianHenryIE\\\\Strauss\\\\League\\\\Container\\\\",
+            $composerAutoloadString
+        );
+    }
+
+    public function test_does_edit_autoloader_namespaces_when_deleting_files(): void
+    {
+        $composerJsonString = <<<'EOD'
+{
+  "name": "brianhenryie/strauss",
+  "require": {
+    "league/container": "*"
+  },
+  "extra": {
+    "strauss": {
+      "namespace_prefix": "BrianHenryIE\\Strauss\\",
+      "classmap_prefix": "BrianHenryIE_Strauss_",
+      "target_directory": "vendor-prefixed",
+      "delete_vendor_packages": true
+    }
+  }
+}
+EOD;
+
+        file_put_contents($this->testsWorkingDir . 'composer.json', $composerJsonString);
+
+        chdir($this->testsWorkingDir);
+
+        exec('composer install');
+
+        $exitCode = $this->runStrauss($output);
+        assert(0 === $exitCode, $output);
+
+        $composerAutoloadString = file_get_contents($this->testsWorkingDir . 'vendor/composer/autoload_psr4.php');
+
+        $this->assertStringNotContainsString(
+            "'League\\\\Container\\\\",
+            $composerAutoloadString
+        );
+
+        $this->assertStringNotContainsString(
+            "BrianHenryIE\\\\Strauss\\\\League\\\\Container\\\\",
+            $composerAutoloadString
+        );
+    }
+
+    public function test_does_edit_autoloader_namespaces_when_target_is_vendor(): void
+    {
+        $composerJsonString = <<<'EOD'
+{
+  "name": "brianhenryie/strauss",
+  "require": {
+    "league/container": "*"
+  },
+  "extra": {
+    "strauss": {
+      "namespace_prefix": "BrianHenryIE\\Strauss\\",
+      "classmap_prefix": "BrianHenryIE_Strauss_",
+      "target_directory": "vendor",
+      "delete_vendor_packages": false
+    }
+  }
+}
+EOD;
+
+        file_put_contents($this->testsWorkingDir . 'composer.json', $composerJsonString);
+
+        chdir($this->testsWorkingDir);
+
+        exec('composer install');
+
+        $exitCode = $this->runStrauss($output);
+        assert(0 === $exitCode, $output);
+
+        $composerAutoloadString = file_get_contents($this->testsWorkingDir . 'vendor/composer/autoload_psr4.php');
+
+        $this->assertStringNotContainsString(
+            "'League\\\\Container\\\\",
+            $composerAutoloadString
+        );
+
+        $this->assertStringContainsString(
+            "BrianHenryIE\\\\Strauss\\\\League\\\\Container\\\\",
+            $composerAutoloadString
+        );
+    }
 }
