8a8b37c9a8c6ba2875e46f8c9ff687219240b9c5
Brian Henry <BrianHenryIE@gmail.com>
2025-07-23 21:11:49 -0700

    Add notes.

--- tests/Integration/DryRunFeatureTest.php
+++ tests/Integration/DryRunFeatureTest.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * @see \Elazar\Flystream\StreamWrapper::stream_open()
+ * needs `if (strpbrk($mode, 'r+') !== false && $mode !== 'w+') {`
+ */
+
+/**
+ * @see \Composer\Autoload\AutoloadGenerator
+ * This line creates the directory on the real filesystem
+ * $packageMap = $this->buildPackageMap($installationManager, $rootPackage, $localRepo->getCanonicalPackages());
+ *
+ * Probably able to address it if every filepath is changed to use a stream wrapper.
+ */
+
 namespace BrianHenryIE\Strauss\Tests\Integration;
 
 use BrianHenryIE\Strauss\Composer\Extra\StraussConfig;
@@ -217,7 +230,9 @@ EOD;
     /**
      * Composer
      *
-     * @see Cleanup\InstalledJson::cleanupVendorInstalledJson()
+     * @see InstalledJson::cleanupVendorInstalledJson()
+     *
+     * php -d allow_url_include=on -r "include 'vendor/autoload.php'; PHPUnit\TextUI\Command::main();" -- --filter=test_composer_files_not_modified
      */
     public function test_composer_files_not_modified(): void
     {
