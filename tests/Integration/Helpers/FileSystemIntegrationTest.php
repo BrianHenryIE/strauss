ca76b135ffc620fe09770fd6dcb30076a0de6142
Brian Henry <BrianHenryIE@gmail.com>
2026-02-21 14:10:29 -0800

    Normalise paths

--- tests/Integration/Helpers/FileSystemIntegrationTest.php
+++ tests/Integration/Helpers/FileSystemIntegrationTest.php
@@ -46,8 +46,8 @@ class FileSystemIntegrationTest extends IntegrationTestCase
 
         mkdir($dir);
 
-        $file1 = $dir . '/file1.php';
-        $file2 = $dir . '/file2.php';
+        $file1 = $fs->normalize($dir . '/file1.php');
+        $file2 = $fs->normalize($dir . '/file2.php');
 
         mkdir($dir . '/subdir');
 
