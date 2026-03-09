2689d7de55362cf59ca3b4e4f81d2477ccc119c4
Brian Henry <BrianHenryIE@gmail.com>
2026-03-08 20:09:04 -0700

    Don't set the classname to an empty string

--- src/Types/ClassSymbol.php
+++ src/Types/ClassSymbol.php
@@ -62,7 +62,7 @@ class ClassSymbol extends DiscoveredSymbol implements AutoloadAliasInterface
     {
         $fqdnOriginalSymbol = $this->fqdnOriginalSymbol;
 
-        while (str_starts_with($fqdnOriginalSymbol, $class_prefix)) {
+        while (str_starts_with($fqdnOriginalSymbol, $class_prefix) && $class_prefix !== $fqdnOriginalSymbol) {
             $fqdnOriginalSymbol = preg_replace('/^'.preg_quote($class_prefix).'/', '', $fqdnOriginalSymbol);
         }
 
