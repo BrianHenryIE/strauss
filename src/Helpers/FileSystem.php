be75c3cd4b168a543f23f45ec887b138ec3dc505
Brian Henry <BrianHenryIE@gmail.com>
2026-04-08 17:32:56 -0700

    Fix drive letter regex

--- src/Helpers/FileSystem.php
+++ src/Helpers/FileSystem.php
@@ -49,7 +49,7 @@ class FileSystem implements FilesystemOperator, FlysystemBackCompatInterface
         $this->workingDir = $workingDir;
 
         $this->pathPrefixer = new PathPrefixer(
-            $flysystemRoot ?? str_contains(PHP_OS, 'WIN') ? preg_replace('/^([a-zA-Z]+:)[\\\/].*/', '$1\\', $workingDir) : '/',
+            $flysystemRoot ?? str_contains(PHP_OS, 'WIN') ? preg_replace('/^([a-zA-Z]+:)[\/].*/', '$1\\', $workingDir) : '/',
             DIRECTORY_SEPARATOR
         );
     }
