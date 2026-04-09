a779c488928c56f2f2a905db91847f9acd2546ab
Brian Henry <BrianHenryIE@gmail.com>
2026-04-08 20:25:56 -0700

    Remove literal `\n` from strings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -64,6 +64,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
         $string = str_replace("\r\n", "\n", $string);
         $string = preg_replace('/^\s*/m', '', $string);
         $string = preg_replace('/\n\s*\n/', "\n", $string);
+        $string = str_replace("\\n", '', $string);
         $string = implode(PHP_EOL, array_map('trim', explode(PHP_EOL, $string)));
         return trim($string);
     }
