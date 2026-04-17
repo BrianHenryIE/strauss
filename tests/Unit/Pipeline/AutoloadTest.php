d362aa2bb233cc33f0b972d3f4599af9ad59bb64
Brian Henry <BrianHenryIE@gmail.com>
2026-04-16 20:24:45 -0700

    Delete code duplicated in parent testcase

--- tests/Unit/Pipeline/AutoloadTest.php
+++ tests/Unit/Pipeline/AutoloadTest.php
@@ -16,19 +16,6 @@ use Psr\Log\Test\TestLogger;
  */
 class AutoloadTest extends TestCase
 {
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        /** @var FilesystemRegistry $registry */
-        $registry = \Elazar\Flystream\ServiceLocator::get(\Elazar\Flystream\FilesystemRegistry::class);
-
-        if ($registry->has('mem')) {
-            $registry->unregister('mem');
-        }
-    }
-
     /**
      * @covers ::generateClassmap
      */
