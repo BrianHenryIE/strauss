50b3d3bfebcab423d80c2438ed542029c5347014
Brian Henry <BrianHenryIE@gmail.com>
2026-07-03 21:44:37 -0700

    Delete unused `PrefixerParseErrorHarness`

--- tests/Unit/PrefixerTest.php
+++ tests/Unit/PrefixerTest.php
@@ -5218,30 +5218,3 @@ EOD;
         $this->assertStringContainsString('$prefix = "\\0BrianHenryIE\TestStrauss\Composer\Autoload\ClassLoader\\0";', $result);
     }
 }
-
-class PrefixerParseErrorHarness extends Prefixer
-{
-    /**
-     * @param array<string,string> $functionReplacementMap
-     */
-    public function callReplaceFunctionsBatch(string $contents, array $functionReplacementMap): string
-    {
-        return $this->replaceFunctionsBatch($contents, $functionReplacementMap);
-    }
-
-    /**
-     * @param array<string,NamespaceSymbol> $namespaceSymbols
-     */
-    public function callReplaceConstFetchNamespacesByMap(array $namespaceSymbols, string $contents): string
-    {
-        return $this->replaceConstFetchNamespacesByMap($namespaceSymbols, $contents);
-    }
-
-    /**
-     * @param NamespaceSymbol[] $namespaceSymbols
-     */
-    public function callPrepareRelativeNamespaces(string $contents, array $namespaceSymbols): string
-    {
-        return $this->prepareRelativeNamespaces($contents, $namespaceSymbols);
-    }
-}
