fe905075b43f5a088ea37e57bc2ecc27b826a056
Brian Henry <BrianHenryIE@gmail.com>
2026-07-05 14:20:19 -0700

    Add dataprovider attribute

--- tests/Integration/Autoload/DumpAutoloadFeatureTest.php
+++ tests/Integration/Autoload/DumpAutoloadFeatureTest.php
@@ -13,6 +13,7 @@ use Composer\Autoload\AutoloadGenerator;
 use Composer\Factory;
 use Composer\IO\NullIO;
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 /**
  * @see DumpAutoload
@@ -22,6 +23,7 @@ class DumpAutoloadFeatureTest extends IntegrationTestCase
     /**
      * @dataProvider provider_optimize_autoloader_for_prefixed_autoload_real
      */
+	#[\PHPUnit\Framework\Attributes\DataProvider('provider_optimize_autoloader_for_prefixed_autoload_real')]
     public function test_optimize_autoloader_for_prefixed_autoload_real(string $composerJsonString, bool $expectAuthoritative): void
     {
         try {
@@ -117,6 +119,7 @@ EOD;
      *
      * @dataProvider provider_fix_double_loading_of_files_autoloaders
      */
+	#[DataProvider('provider_fix_double_loading_of_files_autoloaders')]
     public function test_fix_double_loading_of_files_autoloaders(string $composerJsonString, bool $includeRootAutoload): void
     {
         mkdir($this->testsWorkingDir . '/src');
@@ -209,6 +212,7 @@ EOD;
      *
      * @dataProvider provider_option_include_root_autoload
      */
+	#[DataProvider('provider_option_include_root_autoload')]
     public function test_option_include_root_autoload(string $composerJsonString, bool $expectRootAutoload): void
     {
         mkdir($this->testsWorkingDir . '/src');
